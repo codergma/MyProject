f85296f8aab912c825411e5f8c9353cd573e3a51
codergma <codergma@gmail.com>
2015-11-27 20:31:18 +0800

    测试

--- index9.php
+++ index9.php
@@ -1,11 +1,2 @@
 <?php
-header('content-type:text/html;charset=utf-8;');
-function &test()
-{
-	static $b ='b';
-	$b .='a';
-	return $b;
-}
-$x =test();
-$x = 'x';
-echo $x;
+echo $_SERVER['HTTP_USER_AGENT'];
